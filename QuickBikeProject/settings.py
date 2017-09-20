4e761e0f263a93ebb8055811bdffe06abdf2f7b9
ShudelEV <e.shudel@mail.ru>
2017-09-20 16:46:48 +0300

    [ADD] (1) Django REST Framework/Serialisation

--- QuickBikeProject/settings.py
+++ QuickBikeProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# REST API to only be accessible to admin users
+# To turn on pagination
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
