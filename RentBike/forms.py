d3fb7793bc7dbbc24a66765ca8182bdb4fdac74f
ShudelEV <e.shudel@mail.ru>
2018-01-03 15:55:33 +0300

    [CHANGE] (5) Custom HTML/index     [ADD] Markers to googlemap

--- RentBike/forms.py
+++ RentBike/forms.py
@@ -12,3 +12,9 @@ class PrevOrderForm(forms.Form):
                                   input_formats=['%Y-%m-%dT%H:%M'],
                                   widget=widgets.DateTimeInput(
                                       format='%Y-%m-%dT%H:%M', attrs={'type': 'datetime-local'}))
+    man_type = forms.BooleanField(label='Man', initial=True)
+    man_type_quant = forms.IntegerField(label='Quantity', max_value=10, required=False)
+    woman_type = forms.BooleanField(label='Woman', required=False)
+    woman_type_quant = forms.IntegerField(label='Quantity', max_value=10, required=False)
+    child_type = forms.BooleanField(label='Child', required=False)
+    child_type_quant = forms.IntegerField(label='Quantity', max_value=10, required=False)
